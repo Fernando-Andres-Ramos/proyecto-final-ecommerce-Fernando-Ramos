aa5568f6a74d12ba31c555e1da97154b5b38678d
Fernando Ramos <fernando-ramos94@hotmail.com>
2025-07-08 17:58:03 -0300

    Add authentication to some prived routes from post, update and delete products

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -4,11 +4,18 @@ import 'dotenv/config'
 const secret_key = process.env.JWT_SECRET_KEY
 
 export const authentication = (req,res,next) =>{
-  const token = req.headers['authorization'].split("")[1]
-  if (!token) return res.sendStatus(401)
+
+  const headerAuth = req.headers['authorization']
+  if(!headerAuth) 
+    return res.sendStatus(401)
+
+  const token = headerAuth.split(" ")[1]
+  if (!token) 
+    return res.sendStatus(401)
   
   jwt.verify(token,secret_key,(err)=>{
-    if(err) return res.sendStatus(403)
+    if(err) 
+      return res.sendStatus(403)
     next()
   })
 }
--- src/utils/token-generator.js
+++ src/utils/token-generator.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
 import 'dotenv/config'
 
-const secret_key = process.env.JWT_SECRET_KET
+const secret_key = process.env.JWT_SECRET_KEY
 
 export const generateToken = (userData) =>{
   const user = {id: userData.id, email:userData.email}
